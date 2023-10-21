96a99ed57943e0d840dbabd2ec36a0223fd2876f
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2023-10-21 01:31:06 +0100

    Pause menu

---
+++ Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        // oculta menu de pausa ao abrir a cena
+        pauseMenuPanel.SetActive(false);
+    }
+
+    /*
+     * Se pressiona "ESC", abre o menu de pausa;
+     * Com o menu de pausa ativo, se pressiona "ESC" ou clica no botão de continuar, retorna ao jogo.
+    */
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // desbloqueia este atributo, uma vez que é bloqueado quando a câmera de jogo está ativa
+            Cursor.lockState = CursorLockMode.None;
+
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        // desbloqueia este atributo, uma vez que é bloqueado quando a câmera de jogo está ativa
+        Cursor.lockState = CursorLockMode.None;
+
+        // congela o tempo
+        Time.timeScale = 0;
+
+        isPaused = true;
+        pauseMenuPanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        // volta ao estado normal da câmera de jogo
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // descongela o tempo
+        Time.timeScale = 1;
+
+        isPaused = false;
+        pauseMenuPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        // descongela o tempo
+        Time.timeScale = 1;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
