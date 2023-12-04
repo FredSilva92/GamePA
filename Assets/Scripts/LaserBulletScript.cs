40aa21c3e680b042ad501a387c4e8a1ae059a80b
Pedro Silva <pedrofredsilva@gmail.com>
2023-12-04 20:27:24 +0000

    Laser effect - 3 stages

--- Assets/Scripts/LaserBulletScript.cs
+++ Assets/Scripts/LaserBulletScript.cs
@@ -5,15 +5,33 @@ public class LaserBulletScript : MonoBehaviour
     [SerializeField]
     private float _damage = 20f;
 
+    [SerializeField]
+    private GameObject muzzlePrefab;
+
+    [SerializeField]
+    private GameObject hitPrefab;
+
     public float Damage { get { return _damage; } }
 
     private void Start()
     {
-        Destroy(gameObject, 0.2f);
+        if (muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, transform.rotation);
+            muzzleVFX.transform.forward = transform.forward;
+        }
+        //Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
+        RaycastHit hit;
+        if (hitPrefab != null && Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Instantiate(hitPrefab, transform.position, hit.transform.rotation);
+        }
+
         Destroy(gameObject);
     }
 }
